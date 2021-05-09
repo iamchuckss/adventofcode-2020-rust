d8cc61ecc7f3f3933e3465e96245c6eb5b57c3bb
iamchuckss <chuck017@umn.edu>
2021-05-09 18:29:55 -0500

    Add alternative solution for day09

--- day09/src/ans.rs
+++ day09/src/ans.rs
@@ -1,156 +1,79 @@
 use aoc2020::parse;
 
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::collections::{HashMap, HashSet, VecDeque};
 use std::path::Path;
 use thiserror::Error;
 
-lazy_static! {
-    static ref LUGGAGE_OUTER_RE: Regex = Regex::new(r"^(?P<outer_color>.*) bags contain").unwrap();
-    static ref LUGGAGE_INNER_RE: Regex =
-        Regex::new(r"(?P<qty>\d+) (?P<color>[^,.]*) bags?[.,]").unwrap();
-}
+pub const DEFAULT_PREAMBLE_LEN: usize = 25;
 
-const MY_BAG: &str = "shiny gold";
-
-pub struct LuggageRule {
-    outer_color: String,
-    contents: Vec<(u32, String)>,
-}
-
-impl std::str::FromStr for LuggageRule {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let outer_color = LUGGAGE_OUTER_RE
-            .captures(s)
-            .ok_or_else(|| "could not find outer color".to_string())?["outer_color"]
-            .to_string();
-
-        let contents = LUGGAGE_INNER_RE
-            .captures_iter(s)
-            .map(|capture| {
-                (
-                    capture["qty"]
-                        .parse::<u32>()
-                        .expect("regex guarantees positive integers"),
-                    capture["color"].to_string(),
-                )
-            })
-            .collect();
-
-        Ok(LuggageRule {
-            outer_color,
-            contents,
-        })
+/// True if the tail is the sum of any two of the previous `preamble_len` numbers
+pub fn xmas_valid(items: &[u64], preamble_len: usize) -> bool {
+    if items.len() < preamble_len {
+        return true;
     }
-}
 
-pub fn part1(input: &Path) -> Result<(), Error> {
-    let mut direct_containers: HashMap<String, HashSet<String>> = HashMap::new();
-    for rule in parse::<LuggageRule>(input)? {
-        for (_, contained_bag) in &rule.contents {
-            direct_containers
-                .entry(contained_bag.clone())
-                .or_default()
-                .insert(rule.outer_color.clone());
+    let preamble = &items[items.len().saturating_sub(preamble_len + 1)..items.len() - 1];
+    let tail = items[items.len() - 1];
+
+    for (idx, a) in preamble.iter().enumerate() {
+        for b in &preamble[idx + 1..] {
+            if a + b == tail {
+                return true;
+            }
         }
     }
+    false
+}
 
-    let mut queue: VecDeque<_> = direct_containers[MY_BAG].iter().cloned().collect();
-    let mut all_containers = HashSet::new();
-    while let Some(q) = queue.pop_front() {
-        if all_containers.insert(q.clone()) {
-            // true if the value was not present in the set
-            queue.extend(direct_containers.entry(q).or_default().iter().cloned());
+pub fn find_first_invalid(items: &[u64], preamble_len: usize) -> Option<u64> {
+    for slice_end in preamble_len + 1..=items.len() {
+        if !xmas_valid(&items[..slice_end], preamble_len) {
+            return Some(items[slice_end - 1]);
         }
     }
-
-    println!(
-        "{} bags can eventually contain a {} bag",
-        all_containers.len(),
-        MY_BAG
-    );
-
-    Ok(())
+    None
 }
 
-fn query_rules(rules: &HashMap<String, LuggageRule>, color: &str) -> u64 {
-    let rule = match rules.get(color) {
-        None => return 0,
-        Some(rule) => rule,
-    };
-
-    let mut qty_contained = 0_u64;
-
-    for (qty, color) in &rule.contents {
-        let qty = *qty as u64;
-        qty_contained += qty;
-        qty_contained += qty * query_rules(rules, color);
+// never returns an empty slice
+fn find_slice_with_sum<'a>(items: &'a [u64], target_sum: u64) -> Option<&'a [u64]> {
+    for low in 0..items.len() {
+        let mut running_sum = items[low];
+        for high in low + 1..items.len() {
+            running_sum += items[high];
+            if running_sum == target_sum {
+                return Some(&items[low..=high]);
+            }
+        }
     }
-
-    qty_contained
+    None
 }
 
-pub fn part2(input: &Path) -> Result<(), Error> {
-    let rules: HashMap<_, _> = parse::<LuggageRule>(input)?
-        .map(|rule| (rule.outer_color.clone(), rule))
-        .collect();
-    let total_contained = query_rules(&rules, MY_BAG);
-    println!("my bag contains {} other bags", total_contained);
-
-    Ok(())
+pub fn find_weakness(items: &[u64], target_sum: u64) -> Option<u64> {
+    let summing_slice = find_slice_with_sum(items, target_sum)?;
+    // we know the slice is never empty, so we can just unwrap safely here
+    Some(summing_slice.iter().min().unwrap() + summing_slice.iter().max().unwrap())
 }
 
-fn query_rules_memoize(
-    rules: &HashMap<String, LuggageRule>,
-    memo: &mut HashMap<String, u64>,
-    color: &str,
-) -> u64 {
-    let rule = match rules.get(color) {
-        None => return 0,
-        Some(rule) => rule,
-    };
+type Carryover = (Vec<u64>, u64);
 
-    let mut qty_contained = 0;
-
-    for (qty, color) in &rule.contents {
-        let qty = *qty as u64;
-        qty_contained += qty;
-        let per_color = match memo.get(color) {
-            Some(n) => *n,
-            None => query_rules_memoize(rules, memo, color),
-        };
-        qty_contained += qty * per_color;
-    }
-
-    memo.insert(color.to_string(), qty_contained);
-    qty_contained
+fn compute_first_invalid(input: &Path) -> Result<Carryover, Error> {
+    let items: Vec<u64> = parse(input)?.collect();
+    let invalid = find_first_invalid(&items, DEFAULT_PREAMBLE_LEN).ok_or(Error::NoInvalid)?;
+    Ok((items, invalid))
 }
 
-pub fn exhaustive_quantize(input: &Path, n: usize) -> Result<(), Error> {
-    let rules: HashMap<_, _> = parse::<LuggageRule>(input)?
-        .map(|rule| (rule.outer_color.clone(), rule))
-        .collect();
+pub fn part1(input: &Path) -> Result<Carryover, Error> {
+    let (items, invalid) = compute_first_invalid(input)?;
+    println!("first invalid: {}", invalid);
+    Ok((items, invalid))
+}
 
-    let mut exhaustive_contents = HashMap::new();
-    for color in rules.keys() {
-        query_rules_memoize(&rules, &mut exhaustive_contents, color);
+pub fn part2(input: &Path, mut carryover: Option<Carryover>) -> Result<(), Error> {
+    if carryover.is_none() {
+        carryover = Some(compute_first_invalid(input)?);
     }
-
-    let mut exhaustive: Vec<_> = exhaustive_contents.iter().map(|(k, v)| (v, k)).collect();
-    exhaustive.sort();
-
-    for (n, color) in exhaustive.iter().rev().take(n) {
-        println!("{:>30}: {:6}", color, n);
-    }
-    println!("{:>31}", "...");
-    println!("{:>30}: {:6}", MY_BAG, exhaustive_contents[MY_BAG]);
-    println!("{:>31}", "...");
-    for (n, color) in exhaustive.iter().take(n).rev() {
-        println!("{:>30}: {:6}", color, n);
-    }
+    let (items, invalid) = carryover.expect("we just guaranteed it wasn't none; qed");
+    let weakness = find_weakness(&items, invalid).ok_or(Error::NoSliceWithSum)?;
+    println!("encryption weakness: {}", weakness);
 
     Ok(())
 }
@@ -159,4 +82,61 @@ pub fn exhaustive_quantize(input: &Path, n: usize) -> Result<(), Error> {
 pub enum Error {
     #[error(transparent)]
     Io(#[from] std::io::Error),
+    #[error("no invalid item found")]
+    NoInvalid,
+    #[error("no sequence summing to target found")]
+    NoSliceWithSum,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest(
+        input,
+        expect,
+        case(26, true),
+        case(49, true),
+        case(100, false),
+        case(50, false)
+    )]
+    fn test_validity_25(input: u64, expect: bool) {
+        let mut items: Vec<u64> = (1..=25).collect();
+        items.push(input);
+
+        assert_eq!(xmas_valid(&items, 25), expect);
+    }
+
+    #[rstest(
+        input,
+        expect,
+        case(26, true),
+        case(65, false),
+        case(64, true),
+        case(66, true)
+    )]
+    fn test_26th(input: u64, expect: bool) {
+        let mut items: Vec<u64> = (1..=25).collect();
+        items.swap(0, 19);
+        items.push(45);
+        items.push(input);
+        dbg!(&items);
+
+        assert_eq!(xmas_valid(&items, 25), expect);
+    }
+
+    const SAMPLE_LIST: &[u64] = &[
+        35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,
+    ];
+
+    #[test]
+    fn test_find_first_invalid() {
+        assert_eq!(find_first_invalid(&SAMPLE_LIST, 5), Some(127));
+    }
+
+    #[test]
+    fn test_find_weakness() {
+        assert_eq!(find_weakness(&SAMPLE_LIST, 127), Some(62));
+    }
 }
